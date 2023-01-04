9497a9edebdfe67fa874b9ed2f961fbe10ecbbf2
saikumar <sai999694@gmail.com>
2023-01-04 15:07:35 +0530

    build-spec

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world-commit test",
+            "message": "hello world-commit test-1",
             # "location": ip.text.replace("\n", "")
         }),
     }
